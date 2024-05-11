a230d5881b7163017f2e5b84fec2fdbab07400b4
Jeewantha H.M.S <119792484+shamilajeewantha@users.noreply.github.com>
2024-05-11 12:54:39 +0530

    kafka added

---
+++ kafka/demo/src/main/java/com/example/KafkaProducerExample.java
@@ -0,0 +1,39 @@
+package com.example;
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class KafkaProducerExample {
+    public static void main(String[] args) {
+        // Set up producer properties
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "34.125.101.208:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Create Kafka producer
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        // Send messages
+        String topic = "test-topic";
+        for (int i = 0; i < 10; i++) {
+            String key = Integer.toString(i);
+            String value = "Message " + i;
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception != null) {
+                        System.err.println("Error sending message: " + exception.getMessage());
+                    } else {
+                        System.out.printf("Message sent to topic=%s, partition=%d, offset=%d%n",
+                                metadata.topic(), metadata.partition(), metadata.offset());
+                    }
+                }
+            });
+        }
+
+        // Close producer
+        producer.close();
+    }
+}
